0e3e738f5defc928d493c14cec4c32d09c1e54c8
Laura Nicolas <lauranicolas87@outlook.com>
2024-04-16 12:02:28 +0200

    akri

---
+++ sample_app/deploy_akri_and_mock_camera/akri_deploy.ps1
@@ -0,0 +1,24 @@
+#####################################################################
+### Akrii install
+#####################################################################
+
+# Install Akrii
+$templateBaseUrl = "https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/drops/sample_app/deploy_akri_and_mock_camera/"
+$videoDir = ".\video"
+helm repo add akri-helm-charts https://project-akri.github.io/akri/
+helm repo update
+helm install akri akri-helm-charts/akri `
+--set onvif.discovery.enabled=true `
+--set onvif.configuration.enabled=true `
+--set onvif.configuration.capacity=2 `
+--set onvif.configuration.brokerPod.image.repository='ghcr.io/project-akri/akri/onvif-video-broker' `
+--set onvif.configuration.brokerPod.image.tag='latest'
+ # Copy video scripts
+Write-Host "Downloading video artifacts"
+
+New-Item -Path $videoDir -ItemType directory -Force
+Invoke-WebRequest ($templateBaseUrl + "artifacts/video/video.mp4") -OutFile $videoDir\video.mp4  
+Invoke-WebRequest ($templateBaseUrl + "artifacts/video/video-streaming.yaml") -OutFile $videoDir\video-streaming.yaml
+Invoke-WebRequest ($templateBaseUrl + "artifacts/video/akri-video-streaming-app.yaml") -OutFile $videoDir\akri-video-streaming-app.yaml
+kubectl apply -f $videoDir\akri-video-streaming-app.yaml
+kubectl apply -f $videoDir\video-streaming.yaml  
