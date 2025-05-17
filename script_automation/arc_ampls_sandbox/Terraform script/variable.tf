a46ffadf7aa753abc36d47580d758d414527dca5
Nicolas VACCARO <nvaccaro@squad.fr>
2025-05-17 08:05:50 +0200

    fix(terraform): update variable description for AMPLS sandbox

--- script_automation/arc_ampls_sandbox/Terraform script/variable.tf
+++ script_automation/arc_ampls_sandbox/Terraform script/variable.tf
@@ -1,32 +1,32 @@
 variable "client_id" {
-  description = "Veuillez insérer votre client ID (AppId)"
+  description = "Please enter your client ID (AppId)"
   type        = string
   sensitive   = true
 }
 
 variable "client_secret" {
-  description = "Veuillez insérer votre mot de passe/secret d'application"
+  description = "Please enter your password/application secret"
   type        = string
   sensitive   = true
 }
 
 variable "tenant_id" {
-  description = "Veuillez insérer votre tenant ID Azure AD"
+  description = "Please enter your tenant ID - Entra ID"
   type        = string
 }
 
 variable "subscription_id" {
-  description = "Veuillez insérer votre subscription ID Azure"
+  description = "Please enter your Azure ID subscription"
   type        = string
 }
 
 variable "admin_username" {
-  description = "Nom d'utilisateur de la VM Windows"
+  description = "Windows VM user name"
   type        = string
 }
 
 variable "admin_password" {
-  description = "Mot de passe de la VM Windows"
+  description = "Windows VM password"
   type        = string
   sensitive   = true
 }
